bb41a3fef29f9c37be275b86fe7117501fa76ca5
mian Shargeel <call2poma@gmail.com>
2025-01-10 09:09:29 +0100

    I have added some importent Comments for myself to remember the reason of logic, i used for.

--- scripts/localStorageTemp.js
+++ scripts/localStorageTemp.js
@@ -6,7 +6,7 @@ function init() { //loading in Body-tag at onloada-function
   let content = localStorage.getItem('notesContent');
   
   // If localStorage is empty, use default arrays
-  if (!title || !content) {
+  if (!title || !content) { //it means that either title or content (or both) do not have a value
     setToLocalStorage(); // Save the default arrays to localStorage
   } else {
     // If localStorage has data, load it into the app
@@ -28,8 +28,8 @@ function getFromLocalStorage() { //getting value of all arrays of Notizblock
   let archiveTitle = localStorage.getItem('archiveNotesTitle');
   let archiveContent = localStorage.getItem('archiveNotes'); 
 
-  if (title && content) {
-    notesTitle = JSON.parse(title);
+  if (title && content) {//if both array have values then... 
+    notesTitle = JSON.parse(title); //assigning values to arrays
     notesContent = JSON.parse(content);
   } else {
     notesTitle = []; //if not found Initialize as empty 
@@ -52,6 +52,7 @@ function getFromLocalStorage() { //getting value of all arrays of Notizblock
     archiveNotes = [];
   }
 }
+/*i have called this function in all other Sections-function where there value is updates[toAddNotes(),getNotesToTrash(index), fromTrashToNote(index), getNotesToArchive(index), getNotesForwardToTrash(index), fromArchiveToNote(index)] this because at every single point of change in states of app localStorage will also update with new values. */
 
 function setToLocalStorage() { //seting values of all arrays in local storage
   //  console.log('Saving to localStorage:', { notesTitle, notesContent, trashNotesTitle, trashNotes, archiveNotesTitle, archiveNotes });
